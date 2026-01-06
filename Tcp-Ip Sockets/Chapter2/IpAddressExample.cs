21c9cefd774a5fc152b912304e756cc486c39c32
PatrickStar_bro <15153686+patrickstarbro@user.noreply.gitee.com>
2026-01-06 11:17:26 +0800

    Chapter2 的IpAddressExample.cs

---
+++ Tcp-Ip Sockets/Chapter2/IpAddressExample.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+
+namespace Tcp_Ip_Sockets.Chapter2;
+
+internal static class IpAddressExample
+{
+    public static void PrintHostInfo(string host)
+    {
+        try
+        {
+            // Attempt to resolve DNS for given host or address
+            var hostInfo = Dns.GetHostEntry(host);
+
+            // Display the primary host name
+            Console.WriteLine("\tCanonical Name: " + hostInfo.HostName);
+
+            // Display list of IP addresses for this host
+            Console.Write("\tIP Addresses: ");
+            foreach (var ipaddr in hostInfo.AddressList) Console.Write(ipaddr + " ");
+            Console.WriteLine();
+
+            // Display list of alias names for this host
+            Console.Write("\tAliases: ");
+            foreach (var alias in hostInfo.Aliases) Console.Write(alias + " ");
+            Console.WriteLine("\n");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("\tUnable to resolve host: " + host + "\n");
+        }
+    }
+}
---
+++ Tcp-Ip Sockets/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using Tcp_Ip_Sockets.Chapter2;
+
+namespace Tcp_Ip_Sockets;
+
+internal abstract class Program
+{
+    private static void Main(string[] args)
+    {
+        // Get and print local host info
+        try
+        {
+            Console.WriteLine("Local Host:");
+            var localHostName = Dns.GetHostName();
+            Console.WriteLine("\tHost Name: " + localHostName);
+
+            IpAddressExample.PrintHostInfo(localHostName);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Unable to resolve local host\n");
+        }
+
+        // Get and print info for hosts given on command line
+        foreach (var arg in args)
+        {
+            Console.WriteLine(arg + ":");
+            IpAddressExample.PrintHostInfo(arg);
+        }
+    }
+}
